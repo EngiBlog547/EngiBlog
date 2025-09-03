9d3061b7188a31b2282338d4af1b12edca4a6a7e
EngiBlog <jaxxgamerytmk7@gmail.com>
2025-09-03 00:21:13 -0600

    Main pages

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss'
+
+import { formatBlogPosts } from '../js/utils'
+
+const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true })
+const posts = formatBlogPosts(Object.values(postImportResult))
+
+export const GET = () => rss({
+  stylesheet: 'rss/styles.xsl',
+  title: 'EngiBlog',
+  description: 'Un blog de confianza sobre temas relacionados a la ingeniería',
+  site: import.meta.env.SITE,
+  items: posts.map((post) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.date,
+    description: post.frontmatter.description,
+    customData: `<author>${post.frontmatter.author}</author>`
+  }))
+})
